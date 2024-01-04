8f70c7a2a8b90b940fc17636ccce75e5cc409e93
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-01-04 16:44:47 +0100

    Added some Keychain constants

---
+++ Keychain/KeychainConstants.swift
@@ -0,0 +1,15 @@
+//
+//  KeychainConstants.swift
+//  Keychain
+//
+//  Created by Macbook Air 2017 on 4. 1. 2024..
+//
+
+import Foundation
+
+// Name of the service
+let service = "MySecretService"
+
+enum Errors: Error {
+    case KeychainCreatingError
+}

01ab71758d1dfc8bae03f1efe1f12ef1c7e82b22
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-01-05 21:53:00 +0100

    Minor comment change

--- Keychain/ViewController.swift
+++ Keychain/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // Testing our Keychain Wrapper
+        // Testing Keychain Wrapper
         
         // Setting Data
         do {
